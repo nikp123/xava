1eee7b967e23d78188d617992a9c445b3ae04bf7
nikp123 <pavlica.nikola@gmail.com>
2018-07-05 04:18:54 +0200

    Fixed Windozes framerate lock

--- output/graphical_win.h
+++ output/graphical_win.h
@@ -2,6 +2,8 @@
 #include <windowsx.h>
 #include <GL/gl.h>
 #include <GL/glu.h>
+#define WGL_WGLEXT_PROTOTYPES
+#include <GL/wglext.h>
 #include <dwmapi.h>
 #include <assert.h>
 #include <tchar.h>
@@ -16,7 +18,7 @@
 
 // define functions
 int init_window_win(char *color, char *bcolor, double foreground_opacity, int col, int bgcol, int gradient, char *gradient_color_1, char *gradient_color_2, unsigned int shdw, unsigned int shdw_col, int w, int h);
-void apply_win_settings(int w, int h);
+void apply_win_settings(int w, int h, int framerate);
 int get_window_input_win(int *should_reload, int *bs, double *sens, int *bw, int *w, int *h);
 void draw_graphical_win(int window_height, int bars_count, int bar_width, int bar_spacing, int rest, int gradient, int f[200]);
 void cleanup_graphical_win(void);
