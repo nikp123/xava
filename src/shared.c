3d8d271595f0edc8a734f3cec6f034f9f5779f8d
Đoàn Trần Công Danh <congdanhqx@gmail.com>
2020-08-27 07:38:26 +0700

    GetInstallDir: fix buffer overflow
    strlen(3) will return length of input, not including terminating NUL
    character. And strcpy(3) will copy the included NUL character.
    Thus, we'll get buffer overflow for 1 character.
    
    Fix it.

--- src/shared.c
+++ src/shared.c
@@ -107,7 +107,7 @@ char *xavaGetInstallDir() {
 		path[strlen(path)-executableNameSize] = '\0';
 	#else 
 		// everything non-windows is simple as fuck, go look at the mess above
-		const char *path = malloc(strlen(PREFIX"/share/"PACKAGE"/"));
+		const char *path = malloc(strlen(PREFIX"/share/"PACKAGE"/") + 1);
 		strcpy(path, PREFIX"/share/"PACKAGE"/");
 	#endif
 	return path;
