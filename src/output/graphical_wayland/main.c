a8adf967f6883476b2638361ff8d57f8aa78c1e0
Nikola Pavlica <pavlica.nikola@gmail.com>
2021-03-08 21:35:11 +0100

    [bugfix] get blending to work better >:(

--- src/output/graphical_wayland/main.c
+++ src/output/graphical_wayland/main.c
@@ -425,17 +425,11 @@ int xavaOutputApply(void *v) {
 	}
 
 	// handle colors
-	p->col = (p->col&0x00ffffff) | ((uint32_t)
-		(p->foreground_opacity*0xff)<<24);
-	p->bgcol = (p->bgcol&0x00ffffff) | ((uint32_t)
-		(p->background_opacity*0xff)<<24);
-
 	p->col = wayland_color_blend(p->col, p->foreground_opacity*255);
+	p->col = wayland_color_blend(p->bgcol, p->background_opacity*255);
 
 	// clean screen because the colors changed
 	xavaOutputClear(v);
-
-
 
 	return EXIT_SUCCESS;
 }
--- src/output/graphical_wayland/render.c
+++ src/output/graphical_wayland/render.c
@@ -1,6 +1,6 @@
 #include "render.h"
 
-uint32_t extern inline wayland_color_blend(uint32_t color, uint16_t alpha) {
+uint32_t extern wayland_color_blend(uint32_t color, uint16_t alpha) {
 	uint8_t red =   ((color >> 16 & 0xff) | (color >> 8 & 0xff00)) * alpha / 0xffff;
 	uint8_t green = ((color >>  8 & 0xff) | (color >> 0 & 0xff00)) * alpha / 0xffff;
 	uint8_t blue =  ((color >>  0 & 0xff) | (color << 8 & 0xff00)) * alpha / 0xffff;
