2910bcb00919b62d586a22370a22093430323bf5
Nikola Pavlica <pavlica.nikola@gmail.com>
2021-08-26 14:53:34 +0200

    [workaround] PortAudio's not playing nice today
    Signed-off-by: Nikola Pavlica <pavlica.nikola@gmail.com>

--- src/input/portaudio/build.cmake
+++ src/input/portaudio/build.cmake
@@ -1,6 +1,11 @@
 # The project default
 option(PORTAUDIO "PORTAUDIO" ON)
 
+if((NOT IM_SURE_THAT_I_WANT_PORTAUDIO_ON_WINDOWS) AND (MSYS OR MINGW OR MSVC))
+	message(WARNING "Static linker's not playing nice on Windows, therefore PortAudio is disabled. If you REALLY want PortAudio set `-DIM_SURE_THAT_I_WANT_PORTAUDIO_ON_WINDOWS:BOOL=ON`")
+	set(PORTAUDIO OFF)
+endif()
+
 if(PORTAUDIO)
 	pkg_check_modules(PORTAUDIO QUIET portaudio-2.0)
 	if(PORTAUDIO_FOUND)
