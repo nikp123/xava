18089fbfd532fc4788a8e9672bf5b294026b21ba
Nikola Pavlica <pavlica.nikola@gmail.com>
2021-12-07 01:31:43 +0100

    [bugfix] fix NSIS compile issue

--- src/cmake/os-prerequisites.cmake
+++ src/cmake/os-prerequisites.cmake
@@ -10,13 +10,13 @@ if(MINGW)
         set(CMAKE_SUPPORT_WINDOWS_EXPORT_ALL_SYMBOLS 1)
     endif()
 
+    # Do not convert the file paths to Windows-native ones as we're building in Linux
+    file(TO_NATIVE_PATH "${CMAKE_CURRENT_SOURCE_DIR}/example_files" xava_EXAMPLE_FILES)
+    string(REPLACE "/" "\\" xava_EXAMPLE_FILES_NSIS "${CMAKE_CURRENT_SOURCE_DIR}/example_files")
+    #string(REPLACE "/" "\\" xava_SOURCE_DIR    "${CMAKE_CURRENT_SOURCE_DIR}")
+
     # Prepare NSI file for compilation
     configure_file("assets/windows/xava.nsi.template" "xava.nsi" NEWLINE_STYLE CRLF)
-
-    # Do not convert the file paths to Windows-native ones as we're building in Linux
-    #file(TO_NATIVE_PATH "${CMAKE_CURRENT_SOURCE_DIR}/example_files" xava_EXAMPLE_FILES)
-    #string(REPLACE "/" "\\" xava_EXAMPLE_FILES "${CMAKE_CURRENT_SOURCE_DIR}/example_files")
-    #string(REPLACE "/" "\\" xava_SOURCE_DIR    "${CMAKE_CURRENT_SOURCE_DIR}")
 
     set(xava_EXECUTABLE_WINDOWS_ICO_PATH "${CMAKE_CURRENT_SOURCE_DIR}/assets/windows/xava.ico")
     configure_file("assets/windows/xava.rc.template" "xava.rc" NEWLINE_STYLE CRLF)
