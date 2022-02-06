39e71ae4753d84657d77be0f7ca9072669abff4c
Nikola Pavlica <pavlica.nikola@gmail.com>
2022-02-07 00:24:44 +0100

    [feature] generate proper icons for the install on XDG
    You'll need imagemagick however...

--- src/cmake/install_and_configure.cmake
+++ src/cmake/install_and_configure.cmake
@@ -24,6 +24,11 @@ find_and_copy_dlls(xava)
 include("src/cmake/copy_gl_shaders.cmake")
 
 if(UNIX AND NOT APPLE)
+    execute_process (COMMAND
+        convert -size 128x128 -density 1200 -background none -format png32
+        "${CMAKE_CURRENT_SOURCE_DIR}/assets/linux/xava.svg"
+        "${CMAKE_CURRENT_BINARY_DIR}/xava.png")
     install (FILES assets/linux/xava.desktop DESTINATION share/applications)
-    install (FILES assets/linux/xava.svg DESTINATION share/icons/hicolor/scalable/apps)
+    install (FILES assets/linux/xava.svg RENAME xava_visualizer.svg DESTINATION share/icons/hicolor/scalable/apps)
+    install (FILES "${CMAKE_CURRENT_BINARY_DIR}/xava.png" RENAME xava_visualizer.png DESTINATION share/icons/hicolor/128x128/apps)
 endif()
