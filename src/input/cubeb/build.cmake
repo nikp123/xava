3e85f82ade22e80df90946500f5133d589538952
Nikola Pavlica <pavlica.nikola@gmail.com>
2022-03-26 20:18:12 +0100

    [bugfix] clean that build up

--- src/input/cubeb/build.cmake
+++ src/input/cubeb/build.cmake
@@ -3,12 +3,12 @@ option(CUBEB "CUBEB" ON)
 
 # libcubeb-0.3
 if(CUBEB)
-    set(BUILD_SHARED_LIBS OFF)
+    set(BUILD_SHARED_LIBS  ON)
     set(BUILD_TESTS       OFF)
     set(BUILD_RUST_LIBS    ON)
     set(BUILD_TOOLS       OFF)
     set(BUNDLE_SPEEX      OFF)
-    set(LAZY_LOAD_LIBS     ON)
+    set(LAZY_LOAD_LIBS    OFF)
     set(USE_SANITIZERS    OFF)
 
     execute_process(COMMAND git submodule update --init --recursive
@@ -17,6 +17,8 @@ if(CUBEB)
 
     add_subdirectory(lib/cubeb)
     set_target_properties(cubeb PROPERTIES POSITION_INDEPENDENT_CODE ON)
+
+    file(REMOVE "${CMAKE_CURRENT_BINARY_DIR}/lib/cubeb/cmake_install.cmake")
 
     add_definitions(-DCUBEB)
     add_library(in_cubeb SHARED "${XAVA_MODULE_DIR}/main.c"
