e1d52fa7baac624f5a041953fd4fec98bfd2f888
nikp123 <nikp123@e.email>
2024-07-10 10:05:29 +0000

    [bugfix] check for librsvg and imagemagick before building the icons

--- src/cmake/install_and_configure.cmake
+++ src/cmake/install_and_configure.cmake
@@ -24,10 +24,19 @@ find_and_copy_dlls(xava)
 include("src/cmake/copy_gl_shaders.cmake")
 
 if(UNIX AND NOT APPLE)
+    # I am lazy and I just want to convert the icons into a format most desktops will be fine with
+    find_program(CONVERT magick
+                 REQUIRED)
+
+    # Despite the program not being called, we need it because it's called by magick
+    find_program(LIBRSVG rsvg-convert
+                 REQUIRED)
+
     execute_process (COMMAND
-        convert -size 128x128 -density 1200 -background none -format png32
+        "${CONVERT}" convert -size 128x128 -density 1200 -background none -format png32
         "${CMAKE_CURRENT_SOURCE_DIR}/assets/linux/xava.svg"
-        "${CMAKE_CURRENT_BINARY_DIR}/xava.png")
+        "${CMAKE_CURRENT_BINARY_DIR}/xava.png"
+         COMMAND_ERROR_IS_FATAL ANY)
     install (FILES assets/linux/xava.desktop DESTINATION share/applications)
     install (FILES assets/linux/xava.svg RENAME xava_visualizer.svg DESTINATION share/icons/hicolor/scalable/apps)
     install (FILES "${CMAKE_CURRENT_BINARY_DIR}/xava.png" RENAME xava_visualizer.png DESTINATION share/icons/hicolor/128x128/apps)
